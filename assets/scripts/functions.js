ff30f5d2a57f862409a5e253abb9573dad8f8db1
Brice Maignan <mgnn.brice@pm.me>
2023-09-17 22:46:49 +0200

    enhanced html/css Template and improved JS display function

--- assets/scripts/functions.js
+++ assets/scripts/functions.js
@@ -1,17 +1,27 @@
 import * as main from "./main.js"
- 
 
-export function createOrderEntry(object, quantity){
-    let item = {
-        id: object.id,
-        name: object.name,
-        unitPrice: object.unitPrice,
-        stockQty: object.stockQty,
-        orderQty: quantity
-    }
-    return item;
-}
 
+export function createOrderEntry(object, quantity) {
+    if (object.stockQty >= quantity) {
+        let item = {
+            id: object.id,
+            name: object.name,
+            unitPrice: object.unitPrice,
+            stockQty: object.stockQty,
+            orderQty: quantity
+        };
+        return item;
+    };
+};
+
+
+export function getObjectById(array, id){
+    parseInt(id);
+    array.map(item => {
+        if(parseInt(item.id) !== id) return;
+        return item;
+    });
+};
 
 
 
